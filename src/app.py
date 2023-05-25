dfacef04a148f37cf4810ca4b99da519f952e1e0
NikoDemo96 <chnicolas.aranguiz@gmail.com>
2023-05-25 01:03:18 +0000

    First version

--- src/app.py
+++ src/app.py
@@ -25,18 +25,44 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+@app.route("/member/<int:id>", methods=["GET"])
+def get_member(id):
+        try:
+            member = jackson_family.get_member(id)
+
+            if member is None:
+                raise APIException("Member not found", status_code=400)
+
+            return jsonify(member), 200
+        except APIException as e:
+            return jsonify(e.to_dict()), 500
+
+@app.route("/member", methods=["POST"])
+def create_member():
+    body=request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    
+    jackson_family.add_member(body)
+    return jsonify({"done": True}), 200
+
+@app.route("/member/<int:id>", methods=["DELETE"])
+def delete_member_by_id(id):
+    jackson_family.delete_member(id)
+    return jsonify({"done": True}), 200
+
+
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
 
 
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
